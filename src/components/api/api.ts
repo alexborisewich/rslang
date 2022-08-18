735466c976782ecdef7b2bb07dab603370dc9860
PiachkoAliaksey <alek17y@mail.ru>
2022-08-18 20:03:11 +0300

    refactor: put base api in Class

--- src/components/api/api.ts
+++ src/components/api/api.ts
@@ -1,146 +1,115 @@
-import { Dictionary,Registration,StatProps,StatBackProps } from "../../common/interface/interface";
+import {Registration,StatBackProps } from "../../common/interface/interface";
 
-const baseApi:string = 'https://rslang-172.herokuapp.com/';
-const wordsApi:string = `${baseApi}words?all=true`;
-const STATISTICS: StatBackProps = {
-  "statistics": {
-    "audioCall": [
-      {
-        "data": "",
-        "learningWords": [],
-        "winStreak": 0,
-        "generalCountLearningWords": 0,
-        "countOfRightAnswers": 0
-      }
-    ],
-    "sprint": [
-      {
-        "data": "0",
-        "learningWords": [],
-        "winStreak": 0,
-        "generalCountLearningWords": 0,
-        "countOfRightAnswers": 0
-      }
-    ]
-  }
-}
+export default class API {
+  baseApi = 'https://rslang-172.herokuapp.com/';
 
+  wordsApi = `${this.baseApi}words?all=true`;
 
-
-export const getDataWords = async():Promise<Array<Dictionary>> => {
-const response = await fetch(`${wordsApi}`);
-
-if(!response.ok){
-  throw new Error(`Could not fetch ${wordsApi}, received ${response.status}`);
-}
-return  await response.json();
-}
-
-export const getDataPage = async(group:string | number,page?:number):Promise<Dictionary[]> => {
-  const response = await fetch(`${baseApi}words?page=${page}&group=${group}`);
-  if (!response.ok) {
-    throw new Error(`Could not fetch ${`${baseApi}words?page=${page}&group=${group}`}, received ${response.status}`);
-  }
-  return await response.json();
-}
-
-export const loginUser = async (values:Registration): Promise<{message:string,userId:string,token:string}> => {
-  const response = await fetch(`${baseApi}signin`,{
-    method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(values)
-  });
-  const content = await response.json();
-
-  if (content.error){
-    throw new Error();
-  }
-  return {message: content.message, userId: content.userId, token:content.token};
-}
-
-
-export const createUser = async (user:Registration):Promise<{id:string,email:string}> => {
-  const rawResponse = await fetch(`${baseApi}users`, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+  STATISTICS: StatBackProps = {
+    statistics: {
+      audioCall: [
+        {
+          data: '',
+          learningWords: [],
+          winStreak: 0,
+          generalCountLearningWords: 0,
+          countOfRightAnswers: 0,
+        },
+      ],
+      sprint: [
+        {
+          data: '0',
+          learningWords: [],
+          winStreak: 0,
+          generalCountLearningWords: 0,
+          countOfRightAnswers: 0,
+        },
+      ],
     },
-    body: JSON.stringify(user)
-  });
-  const content = await rawResponse.json();
+  };
 
-  return{id:content.id, email:content.email }
-};
-
-
-export const getUserWord = async () => {
-  const{userId:id,token:newToken} = await loginUser(values);
-  const url:string = `${baseApi}users/${id}/words/`;
-
-  const rawResponse = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${newToken}`,
-      'Accept': 'application/json',
-    }
-  });
-  if (!rawResponse.ok) {
-    throw new Error(`Could not fetch ${url}, received ${rawResponse.status}`);
+  async getWordsData() {
+    const response = await fetch(`${this.wordsApi}`);
+    return response;
   }
-  return await rawResponse.json();
-};
 
-export const createUserWord = async (userId:string, wordId:string, word:string) => {
-  const{token:newToken} = await loginUser(values);
-  const rawResponse = await fetch(`${baseApi}users/${userId}/words/${wordId}`, {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${newToken}`,
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(word)
-  });
-  const content = await rawResponse.json();
-  return content;
-};
-
-const getStatistics = async (user:{userId:string,token:string}): Promise<StatProps> => {
-
-  const rawResponse = await fetch(`${baseApi}users/${user.userId}/statistics`, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${user.token}`,
-      'Accept': 'application/json',
-    }
-  });
-  if (rawResponse.ok) {
-    const content = await rawResponse.json();
-    if (!content.statistics.audioCall && !content.statistics.sprint) {
-      setStatistics(user, STATISTICS);
-      return STATISTICS.statistics;
-    } else return await content;
-  } else {
-    setStatistics(user, STATISTICS);
-    return STATISTICS.statistics;
+  async getDataPage(group: string | number, page?: number) {
+    const response = await fetch(`${this.baseApi}words?page=${page}&group=${group}`);
+    return response;
   }
-};
 
-const setStatistics = async (user:{userId:string,token:string}, statistic: StatBackProps) => {
-  const rawResponse = await fetch(`${baseApi}users/${user.userId}/statistics`, {
-    method: 'PUT',
-    headers: {
-      'Authorization': `Bearer ${user.token}`,
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(statistic),
-  });
-};
+  async loginUser(body: Registration) {
+    const response = await fetch(`${this.baseApi}signin`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    return response;
+  }
+
+  async createUser(body: Registration) {
+    const response = await fetch(`${this.baseApi}users`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    return response;
+  }
+
+  async getUserWord(id: number, token: string) {
+    const url = `${this.baseApi}users/${id}/words/`;
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+      },
+    });
+    return response;
+  }
+
+  async createUserWord(userId: string, token: string, wordId: string, word: string) {
+    const response = await fetch(`${this.baseApi}users/${userId}/words/${wordId}`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(word),
+    });
+    return response;
+  }
+
+  async getStatistic(user: { userId: string; token: string }) {
+    const response = await fetch(`${this.baseApi}users/${user.userId}/statistics`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+        Accept: 'application/json',
+      },
+    });
+    return response;
+  }
+
+  async setStatistic(user: { userId: string; token: string }, statistic: StatBackProps) {
+    await fetch(`${this.baseApi}users/${user.userId}/statistics`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(statistic),
+    });
+  }
+}
 
 
 
