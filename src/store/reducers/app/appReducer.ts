7391b40f07f7cd1aa4b40d4e87aad213ca8c8f0b
Alexander Naumov <Axeller@mail.ru>
2022-08-23 20:30:12 +0800

    feat: stylize header

--- src/store/reducers/app/appReducer.ts
+++ src/store/reducers/app/appReducer.ts
@@ -1,18 +1,13 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { AppState, Tabs } from '../../../common/types/app/types';
 
-const initialState: AppState = { activeTab: Tabs.DICTIONARY };
-
-// const getWords = createAsyncThunk('app/getWords', () => {
-
-// });
+const initialState: AppState = { activeTab: Tabs.HOMEPAGE };
 
 const appSlice = createSlice({
   name: 'app',
   initialState,
   reducers: {
     switchTab(state, action: PayloadAction<string>) {
-      console.log('switchtab');
       state.activeTab = action.payload;
     },
   },
