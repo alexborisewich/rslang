535bfd23f874027083e8b8b77eaa609a5f3c9886
PiachkoAliaksey <alek17y@mail.ru>
2022-08-25 11:24:37 +0300

    feat: interfaces from sprint game

--- src/common/interface/interface.ts
+++ src/common/interface/interface.ts
@@ -14,6 +14,22 @@ export interface Dictionary {
   textMeaningTranslate: string;
   wordTranslate: string;
 }
+export interface WordDictionary {
+  id: string;
+  group: number;
+  page: number;
+  word: string;
+  image: string;
+  audio: string;
+  audioMeaning: string;
+  audioExample: string;
+  textMeaning: string;
+  textExample: string;
+  transcription: string;
+  textExampleTranslate: string;
+  textMeaningTranslate: string;
+  wordTranslate: string;
+}
 
 export interface Registration {
   email: string;
@@ -36,3 +52,11 @@ export interface StatProps {
 export interface StatBackProps {
   statistics: StatProps;
 }
+
+export interface Game{
+  words: WordDictionary[];
+  hardWords: string[];
+  hard: string;
+  group:number;
+  page:number;
+  }
