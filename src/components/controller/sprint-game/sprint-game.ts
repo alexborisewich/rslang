819389f2b0031452edc1ec8fdd055cb45f1b12c4
PiachkoAliaksey <alek17y@mail.ru>
2022-08-27 22:13:26 +0300

    refactor: event listener and objects

--- src/components/controller/sprint-game/sprint-game.ts
+++ src/components/controller/sprint-game/sprint-game.ts
@@ -1,9 +1,12 @@
 import { WordDictionary, Game } from '../../../common/interface/interface';
 import { getRandomArrId, getRandomBoolean, getRandomInteger, playSound } from '../../../common/utils/utils';
-import api from '../../api/api';
+// import api from '../../api/api';
+import store from '../../../store/store';
+import { switchTab } from '../../../store/reducers/app/appReducer';
+import { objectJSON, LogicGames } from '../games/games';
 
-const response = await api.getDataPage(1, 2);
-const objectJSON = (await response.json()) as WordDictionary[];
+// const response = await api.getDataPage(Number(startNewLogic.numberOfPage), 1);
+// const objectJSON = (await response.json()) as WordDictionary[];
 
 // page1 group 1
 
@@ -18,10 +21,21 @@ class LogicSprintGame {
     const sprintBackBlock = document.querySelector('.sprint__back') as HTMLButtonElement;
     const btnTrue = document.querySelector('.sprint__answer-btn-true') as HTMLButtonElement;
     const btnFalse = document.querySelector('.sprint__answer-btn-false') as HTMLButtonElement;
+    const closeGame = document.querySelector('.games__close-btn');
+    const answerResult = document.querySelector('.games__finish') as HTMLElement;
+    const backToGames = document.querySelector('.sprint__back-to-list-games');
+
+    closeGame?.addEventListener('click', function () {
+      store.dispatch(switchTab('homepage'));
+    });
+    backToGames?.addEventListener('click', function () {
+      store.dispatch(switchTab('games'));
+      const startLogic = new LogicGames();
+      startLogic.createLogic();
+    });
+
     btnTrue.disabled = true;
     btnFalse.disabled = true;
-
-
 
     let WORDS_GROUP: WordDictionary[];
     let WORDS_GAME: WordDictionary[];
@@ -40,11 +54,11 @@ class LogicSprintGame {
     let errorList: WordDictionary[] = [];
 
     const objectGames: Game = {
-      words: objectJSON,
+      words: objectJSON.object,
       hardWords: [],
       hard: '',
-      group: objectJSON[0].group,
-      page: objectJSON[0].page,
+      group: objectJSON.object[0].group,
+      page: objectJSON.object[0].page,
     };
 
     // page1 group 1
@@ -58,29 +72,11 @@ class LogicSprintGame {
       return WORDS_GROUP;
     };
 
-    window.onload = function () {
-      sprintBackBlock.style.display = 'none';
-      if (!timerOn) {
-        timerOn = true;
-        const animation = setInterval(function () {
-          if (Number(timerSprint!.textContent) === 0) {
-            clearInterval(animation);
-            timerOn = false;
-            sprintBackBlock.style.display = 'flex';
-            alert(score);
-          } else {
-            timerSprint!.textContent = `${Number(timerSprint!.textContent) - 1}`;
-          }
-        }, 1000);
-      }
-    };
-
     const checkArrLength = (object: Game) => {
       if (object.words.length) {
         WORDS_GROUP = getWordsGroup();
         WORDS_GAME = getWordsGame();
         indexesWord = getRandomArrId(WORDS_GAME.length);
-        console.log(WORDS_GAME.length);
         idWord = indexesWord.pop();
         idTranslate = getRandomBoolean() ? idWord : getRandomInteger(WORDS_GAME.length);
         wordInEnglish!.textContent = WORDS_GAME[idWord!].word;
@@ -180,7 +176,59 @@ class LogicSprintGame {
             sprintBackBlock.style.display = 'flex';
             btnTrue.disabled = true;
             btnFalse.disabled = true;
-            alert(`Score: ${score}`);
+            answerResult.innerHTML = `<div class="games__finish-container">
+            <h3 class="games__finish-title">Ваш результат: ${score} очков</h3>
+            <h4 class="games__finish-title"> Длина серии: ${series} </h4>
+            <div class="games__answers-counters">
+            <span class="games__answers-right">Знаю:
+              <span class="games__correct-count">${correctList.length}</span>
+            </span>
+            <span class="games__answers-wrong">Не знаю:
+              <span class="games__wrong-count">${errorList.length}</span>
+            </span>
+            </div>
+            <table class="games__finish-statistic">
+              <thead>
+                <tr>
+                  <th>1</th>
+                  <th>2</th>
+                  <th>3</th>
+                </tr>
+              </thead>
+              <tbody class = "tbody-items">
+              <tr>
+              <td>англ. слово</td>
+              <td>[]</td>
+              <td>рус. слово</td>
+            </tr>
+            <tr>
+            <td colspan="3" style = "text-align: center; background-color:#00FF00"> Изученные</td>
+            </tr>
+            ${correctList.map((word: WordDictionary) => {
+              return `<tr>
+                <td>${word.word}</td>
+                <td>${word.transcription}</td>
+                <td>${word.wordTranslate}</td>
+              </tr>`;
+            })}
+            <tr>
+            <td colspan="3" style = "text-align: center; background-color:#FF0000"> Не изученные</td>
+            </tr>
+            ${errorList.map((word: WordDictionary) => {
+              return `<tr>
+                <td>${word.word}</td>
+                <td>${word.transcription}</td>
+                <td>${word.wordTranslate}</td>
+              </tr>`;
+            })}
+              </tbody>
+            </table>
+            <button class="games__exit-btn btn">Выйти</button>
+          </div>`;
+            answerResult.style.display = 'flex';
+            document.querySelector('.games__exit-btn')?.addEventListener('click', function () {
+              answerResult.style.display = 'none';
+            });
           } else {
             timerSprint!.textContent = `${Number(timerSprint!.textContent) - 1}`;
           }
