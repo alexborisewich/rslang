dd1d1d8aa9bf0bfdea7a71dc98452d2cdac857df
Alexander Naumov <Axeller@mail.ru>
2022-08-29 02:31:15 +0800

    feat: add user answers logic and games page

--- src/components/controller/sprint/SprintController.ts
+++ src/components/controller/sprint/SprintController.ts
@@ -1,16 +1,40 @@
 import { Dictionary } from '../../../common/interface/interface';
+import {
+  checkUserAnswer,
+  getSprintData,
+  setRoundState,
+  switchGameStatus,
+} from '../../../store/reducers/sprint/sprintReducer';
 import store from '../../../store/store';
 
 export default class SprintController {
   state = store.getState().sprint;
 
   startGame() {
-    const question = this.getRandomWord(this.state.words);
-    const answer = Math.random() > 0.5 ? question.wordTranslate : this.getRandomWord(this.state.words).wordTranslate;
-    // store.dispatch(setQuestion(question));
+    store.dispatch(getSprintData({ group: this.state.group, page: this.state.page })).then(() => {
+      store.dispatch(switchGameStatus(true));
+      this.startRound();
+    });
   }
 
-  finishGame() {}
+  startRound() {
+    const { words } = store.getState().sprint;
+    if (words.length) {
+      const question = this.getRandomWord(words);
+      const answer = Math.random() > 0.5 ? question : this.getRandomWord(words);
+      store.dispatch(setRoundState({ question, answer }));
+    } else this.finishGame();
+  }
+
+  setUserAnswer(answer: boolean) {
+    store.dispatch(checkUserAnswer(answer));
+    this.startRound();
+  }
+
+  finishGame() {
+    console.log('game is finished');
+    store.dispatch(switchGameStatus(false));
+  }
 
   getRandomWord = (array: Dictionary[]) => {
     const idx = Math.floor(Math.random() * array.length);
--- src/components/view/mini-games/sprint/Sprint.ts
+++ src/components/view/mini-games/sprint/Sprint.ts
@@ -5,31 +5,37 @@ export default class Sprint {
 
   createElement() {
     return `<section class="sprint">
+      <div class="sprint__game">
       <div class="games__btn-wpapper">
           <button class="games__sound-btn">Звук</button>
           <button class="games__screen-btn">На весь экран</button>
           <button class="games__close-btn">&#9587</button>
       </div>
       <div class = "sprint__body">
+      <div class="sprint__interface ${this.sprintState.isStarted ? '' : 'hidden'}">
       <div class = "sprint__header">
           <div class = "sprint__score">${this.sprintState.score}</div>
           <div class = "sprint__timer">${this.sprintState.time}</div>
       </div>
   
-      <div class = "sprint__word"></div>
-      <div class = "sprint__translate"></div>
+      <div class = "sprint__word">${this.sprintState.roundState.question?.word}</div>
+      <div class = "sprint__translate">${this.sprintState.roundState.answer?.wordTranslate}</div>
       <div class = "sprint__answer">
-          <button class = "sprint__answer-btn sprint__answer-btn-false">&#10008</button>
-          <button class = "sprint__answer-btn sprint__answer-btn-true">&#10004</button>
+          <button class = "sprint__answer-btn sprint__answer-btn-false" id="sprint-answer-false">&#10008</button>
+          <button class = "sprint__answer-btn sprint__answer-btn-true" id="sprint-answer-true">&#10004</button>
       </div>
+      </div>
+      
       <div class = "sprint__back">
-      <button class = "sprint__back-to-new-game">Новая игра</button>
+      <button class = "sprint__back-to-new-game" id="sprint-new-game">Новая игра</button>
       <button class = "sprint__back-to-list-games">К списку игр</button>
       </div>
     </div>
     <div class="games__finish">
   
     </div>
+      </div>
+      
     </section>`;
   }
 }
