dc6fc62e7cc6f0560f91a0f57b8573a5a3e13f75
Aliaksei Barysewich <alexboris61529@gmail.com>
2022-08-30 10:51:24 +0300

    feat: add values for games levels inputs

--- src/components/view/mini-games/Games.ts
+++ src/components/view/mini-games/Games.ts
@@ -2,11 +2,6 @@ export default class Games {
   createElement() {
     return `<section class="games">
       <div class="games__container container">
-        <div class="games__btn-wpapper">
-          <button class="games__sound-btn">Звук</button>
-          <button class="games__screen-btn">На весь экран</button>
-          <button class="games__close-btn">Закрыть</button>
-        </div>
         <div class="games__select">
           <a class="games__audiochallenge-link game-link" href="#">
             <span class="game-link__title">Аудиовызов</span>
@@ -14,50 +9,50 @@ export default class Games {
               <img class="game-link__img" src="" alt="">
             </span>
           </a>
-          <ul class="games__level-list">
-            <li class="games__level-item">
-              <input class="games__level-input" type="radio" id="A1" name="level-select" checked>
-              <label class="games__level-label" for="A1">
-                <span class="games__level-title">A1</span>
-                <span class="games__level-name">Elementary</span></label>
-            </li>
-            <li class="games__level-item">
-              <input class="games__level-input" type="radio" id="A2" name="level-select">
-              <label class="games__level-label" for="A2">
-                <span class="games__level-title">A2</span>
-                <span class="games__level-name">Pre-Intermediate</span>
-              </label>
-            </li>
-            <li class="games__level-item">
-              <input class="games__level-input" type="radio" id="B1" name="level-select">
-              <label class="games__level-label" for="B1">
-                <span class="games__level-title">B1</span>
-                <span class="games__level-name">Intermediate</span>
-              </label>
-            </li>
-            <li class="games__level-item">
-              <input class="games__level-input" type="radio" id="B2" name="level-select">
-              <label class="games__level-label" for="B2">
-                <span class="games__level-title">B2</span>
-                <span class="games__level-name">Upper-Intermediate</span>
-              </label>
-            </li>
-            <li class="games__level-item">
-              <input class="games__level-input" type="radio" id="C1" name="level-select">
-              <label class="games__level-label" for="C1">
-                <span class="games__level-title">C1</span>
-                <span class="games__level-name">Advanced</span>
-              </label>
-            </li>
-            <li class="games__level-item">
-              <input class="games__level-input" type="radio" id="C2" name="level-select">
-              <label class="games__level-label" for="C2">
-                <span class="games__level-title">C2</span>
-                <span class="games__level-name">Proficiency</span>
-              </label>
-            </li>
-          </ul>
-          <a class="games__audiochallenge-link game-link" href="#">
+            <ul class="games__level-list">
+              <li class="games__level-item">
+                <input class="games__level-input" type="radio" value="0" name="level-select" checked>
+                <label class="games__level-label" for="A1">
+                  <span class="games__level-title">A1</span>
+                  <span class="games__level-name">Elementary</span></label>
+              </li>
+              <li class="games__level-item">
+                <input class="games__level-input" type="radio" value="1" name="level-select">
+                <label class="games__level-label" for="A2">
+                  <span class="games__level-title">A2</span>
+                  <span class="games__level-name">Pre-Intermediate</span>
+                </label>
+              </li>
+              <li class="games__level-item">
+                <input class="games__level-input" type="radio" value="2" name="level-select">
+                <label class="games__level-label" for="B1">
+                  <span class="games__level-title">B1</span>
+                  <span class="games__level-name">Intermediate</span>
+                </label>
+              </li>
+              <li class="games__level-item">
+                <input class="games__level-input" type="radio" value="3" name="level-select">
+                <label class="games__level-label" for="B2">
+                  <span class="games__level-title">B2</span>
+                  <span class="games__level-name">Upper-Intermediate</span>
+                </label>
+              </li>
+              <li class="games__level-item">
+                <input class="games__level-input" type="radio" value="4" name="level-select">
+                <label class="games__level-label" for="C1">
+                  <span class="games__level-title">C1</span>
+                  <span class="games__level-name">Advanced</span>
+                </label>
+              </li>
+              <li class="games__level-item">
+                <input class="games__level-input" type="radio" value="5" name="level-select">
+                <label class="games__level-label" for="C2">
+                  <span class="games__level-title">C2</span>
+                  <span class="games__level-name">Proficiency</span>
+                </label>
+              </li>
+            </ul>
+          <a class="games__sprint-link game-link" href="#">
             <span class="game-link__title">Спринт</span>
             <span class="game-link__img-wrapper">
               <img class="game-link__img" src="" alt="">
