fb3b3f6c5dd417213690f663d5b1d32acf0d8e08
Aliaksei Barysewich <alexboris61529@gmail.com>
2022-09-02 17:55:26 +0300

    feat: add new user token fetch

--- src/components/api/api.ts
+++ src/components/api/api.ts
@@ -1,41 +1,54 @@
 import { Registration, StatBackProps } from '../../common/interface/interface';
+import { getJwtToken, getRefreshToken, getUserId } from '../../common/utils/utils';
+import AuthorizationHandler from '../view/authorization/AuthorizationHandler';
 
 class API {
   baseApi = 'https://rslang-172.herokuapp.com/';
 
-  wordsApi = `${this.baseApi}words?all=true`;
-
-  STATISTICS: StatBackProps = {
-    statistics: {
-      audioCall: [
-        {
-          data: '',
-          learningWords: [],
-          winStreak: 0,
-          generalCountLearningWords: 0,
-          countOfRightAnswers: 0,
-        },
-      ],
-      sprint: [
-        {
-          data: '0',
-          learningWords: [],
-          winStreak: 0,
-          generalCountLearningWords: 0,
-          countOfRightAnswers: 0,
-        },
-      ],
-    },
-  };
-
-  async getWordsData() {
-    const response = await fetch(`${this.wordsApi}`);
-    return response;
-  }
-
   async getDataPage(group: string | number, page?: number) {
     const response = await fetch(`${this.baseApi}words?page=${page}&group=${group}`);
     return response;
+  }
+
+  async getDataWord(wordID: string) {
+    const response = await fetch(`${this.baseApi}words/${wordID}`);
+    return response;
+  }
+
+  // ---USER---
+
+  async createUser(body: Registration) {
+    const response = await fetch(`${this.baseApi}users`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    return response;
+  }
+
+  async getUser() {
+    const response = await fetch(`${this.baseApi}users/${getUserId()}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${getJwtToken()}`,
+        Accept: 'application/json',
+      },
+    });
+    return response;
+  }
+
+  async getNewUserToken() {
+    const response = await fetch(`${this.baseApi}users/${getUserId()}/tokens`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${getRefreshToken()}`,
+        Accept: 'application/json',
+      },
+    });
+    return response.ok ? response : AuthorizationHandler.logOut();
   }
 
   async loginUser(body: Registration) {
@@ -50,47 +63,24 @@ class API {
     return response;
   }
 
-  async getUser(id: number, token: string) {
-    const url = `${this.baseApi}users/${id}`;
-    const response = await fetch(url, {
+  // ---USER WORDS
+
+  async getUserWord() {
+    const response = await fetch(`${this.baseApi}users/${getUserId()}/words/`, {
       method: 'GET',
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${getJwtToken()}`,
         Accept: 'application/json',
       },
     });
     return response;
   }
 
-  async createUser(body: Registration) {
-    const response = await fetch(`${this.baseApi}users`, {
+  async createUserWord(wordId: string, word: string) {
+    const response = await fetch(`${this.baseApi}users/${getUserId()}/words/${wordId}`, {
       method: 'POST',
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    });
-    return response;
-  }
-
-  async getUserWord(id: number, token: string) {
-    const url = `${this.baseApi}users/${id}/words/`;
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        Accept: 'application/json',
-      },
-    });
-    return response;
-  }
-
-  async createUserWord(userId: string, token: string, wordId: string, word: string) {
-    const response = await fetch(`${this.baseApi}users/${userId}/words/${wordId}`, {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${getJwtToken()}`,
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
@@ -99,22 +89,22 @@ class API {
     return response;
   }
 
-  async getStatistic(user: { userId: string; token: string }) {
-    const response = await fetch(`${this.baseApi}users/${user.userId}/statistics`, {
+  async getStatistic() {
+    const response = await fetch(`${this.baseApi}users/${getUserId()}/statistics`, {
       method: 'GET',
       headers: {
-        Authorization: `Bearer ${user.token}`,
+        Authorization: `Bearer ${getJwtToken()}`,
         Accept: 'application/json',
       },
     });
     return response;
   }
 
-  async setStatistic(user: { userId: string; token: string }, statistic: StatBackProps) {
-    await fetch(`${this.baseApi}users/${user.userId}/statistics`, {
+  async setStatistic(statistic: StatBackProps) {
+    await fetch(`${this.baseApi}users/${getUserId()}/statistics`, {
       method: 'PUT',
       headers: {
-        Authorization: `Bearer ${user.token}`,
+        Authorization: `Bearer ${getJwtToken()}`,
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
