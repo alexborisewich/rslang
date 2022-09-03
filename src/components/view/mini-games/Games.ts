a73a217c5455ae165cb788dec153eb0a35b4a058
PiachkoAliaksey <alek17y@mail.ru>
2022-09-03 20:23:24 +0300

    refactor: change styles and tags

--- src/components/view/mini-games/Games.ts
+++ src/components/view/mini-games/Games.ts
@@ -11,42 +11,42 @@ export default class Games {
         </a>
         <ul class="games__level-list">
           <li class="games__level-item">
-            <input class="games__level-input" type="radio" id="A1" name="level-select" value="0" checked>
             <label class="games__level-label" for="A1">
+            <input class="games__level-input" type="radio" id="A1" name="level-select" value="0" checked>
               <span class="games__level-title">A1</span>
               <span class="games__level-name">Elementary</span></label>
           </li>
           <li class="games__level-item">
-            <input class="games__level-input" type="radio" id="A2" name="level-select" value="1">
             <label class="games__level-label" for="A2">
+            <input class="games__level-input" type="radio" id="A2" name="level-select" value="1">
               <span class="games__level-title">A2</span>
               <span class="games__level-name">Pre-Intermediate</span>
             </label>
           </li>
           <li class="games__level-item">
-            <input class="games__level-input" type="radio" id="B1" name="level-select" value="2">
             <label class="games__level-label" for="B1">
+            <input class="games__level-input" type="radio" id="B1" name="level-select" value="2">
               <span class="games__level-title">B1</span>
               <span class="games__level-name">Intermediate</span>
             </label>
           </li>
           <li class="games__level-item">
-            <input class="games__level-input" type="radio" id="B2" name="level-select" value="3">
             <label class="games__level-label" for="B2">
+            <input class="games__level-input" type="radio" id="B2" name="level-select" value="3">
               <span class="games__level-title">B2</span>
               <span class="games__level-name">Upper-Intermediate</span>
             </label>
           </li>
           <li class="games__level-item">
-            <input class="games__level-input" type="radio" id="C1" name="level-select" value="4">
             <label class="games__level-label" for="C1">
+            <input class="games__level-input" type="radio" id="C1" name="level-select" value="4">
               <span class="games__level-title">C1</span>
               <span class="games__level-name">Advanced</span>
             </label>
           </li>
           <li class="games__level-item">
-            <input class="games__level-input" type="radio" id="C2" name="level-select" value="5">
             <label class="games__level-label" for="C2">
+            <input class="games__level-input" type="radio" id="C2" name="level-select" value="5">
               <span class="games__level-title">C2</span>
               <span class="games__level-name">Proficiency</span>
             </label>
