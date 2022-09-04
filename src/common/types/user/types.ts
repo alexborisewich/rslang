8070de39074ee5e14a254ad671cb0facc4c16ef0
Alexander Naumov <Axeller@mail.ru>
2022-09-04 08:56:31 +0800

    feat: add user statistic interface

--- src/common/types/user/types.ts
+++ src/common/types/user/types.ts
@@ -1,8 +1,22 @@
+import { Dictionary } from '../../interface/interface';
+
 export type UserState = {
   userId: string;
   email: string;
   message: string;
   token: string;
+  statistic: {
+    complexWords: Dictionary[];
+    learnedWords: Dictionary[];
+    audiochallenge: {
+      maxScore: number;
+      correctCount: number;
+    };
+    sprint: {
+      maxScore: number;
+      correctCount: number;
+    };
+  };
   isLoggedOn: boolean;
   isLoading: boolean;
   error: { isError: boolean; message: string };
