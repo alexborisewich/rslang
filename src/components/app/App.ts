3d97dc92a707eda0d9cdfa757008b6d3d5b54f97
Alexander Naumov <Axeller@mail.ru>
2022-09-07 01:54:31 +0800

    feat: add session storage check

--- src/components/app/App.ts
+++ src/components/app/App.ts
@@ -1,3 +1,4 @@
+import { checkUser } from '../../common/utils/utils';
 import { logIn } from '../../store/reducers/user/userReducer';
 import store from '../../store/store';
 import AppView from '../view/appView';
@@ -6,18 +7,8 @@ export default class App {
   view = new AppView();
 
   start() {
-    const storedUser = localStorage.getItem('user');
-    if (storedUser) {
-      const user = JSON.parse(storedUser) as {
-        message: string;
-        name: string;
-        refreshToken: string;
-        token: string;
-        userId: string;
-      };
-      store.dispatch(logIn(user));
-    }
-
+    const storedUser = checkUser();
+    if (storedUser) store.dispatch(logIn(storedUser));
     this.view.renderApp();
     store.subscribe(() => this.view.renderApp());
   }
